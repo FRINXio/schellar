c2015bf0623fdb4580e5d9074834c10fc2b77b4b
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-10-20 12:44:37 +0200

    Add integration testing for mongodb

--- schellar/mongo/mongo_test.go
+++ schellar/mongo/mongo_test.go
@@ -1,4 +1,4 @@
-package postgres
+package mongo
 
 import (
 	"testing"
@@ -6,14 +6,6 @@ import (
 	"github.com/frinx/schellar/ifc"
 	"github.com/frinx/schellar/it"
 )
-
-func TestSqlParamsRange(t *testing.T) {
-	actual := sqlParamsRange(3)
-	expected := "($1,$2,$3)"
-	if actual != expected {
-		t.Fatalf("Unexpected: %v, should be %v", actual, expected)
-	}
-}
 
 func initIntegration(t *testing.T) ifc.DB {
 	db := InitDB()
