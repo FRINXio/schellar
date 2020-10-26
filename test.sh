70666f444416578ec476e2acf1d5f2555c20fd13
Tomas Olvecky <tomas.olvecky@gmail.com>
2020-10-26 13:36:02 +0100

    Add integration tests to github workflow

--- test.sh
+++ test.sh
@@ -3,13 +3,14 @@ dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dirname}
 
 cd schellar
+go test -v -short ./...
+
 docker-compose -f ../docker-compose.test.yml up -d
+
+trap 'docker-compose -f ../docker-compose.test.yml down' err exit
+
 sleep 5
 export BACKEND="postgres"
 export POSTGRES_DATABASE_URL="host=127.0.0.1 port=6432 user=postgres password=postgres database=schellar_test"
-export POSTGRES_MIGRATIONS_DIR=${dirname}/schellar/migrations
-go test -count=1 ./...
-RESULT=$?
-
-docker-compose -f ../docker-compose.test.yml down
-exit $RESULT
+export POSTGRES_MIGRATIONS_DIR="$(pwd)/migrations"
+go test -run Integration ./...
