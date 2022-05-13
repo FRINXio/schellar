384ae63e4b73d013a39394315187b7ad80a77813
Jozef Volak <jvolak@frinx.io>
2022-05-13 11:51:32 +0200

    Fix test, removing network issue

--- test.sh
+++ test.sh
@@ -1,21 +1,30 @@
 set -x
+
+function testSchellar() {
+
+    docker-compose -f ../docker-compose.test.yml up -d
+
+    sleep 5
+    export BACKEND="postgres"
+
+    export POSTGRES_HOST=127.0.0.1
+    export POSTGRES_PORT=6432
+    export POSTGRES_DB=schellar_test
+    export POSTGRES_USER=postgres
+    export POSTGRES_PASSWORD=postgres
+    export POSTGRES_MIGRATIONS_DIR="$(pwd)/migrations"
+    go test -run Integration ./...
+    docker-compose -f ../docker-compose.test.yml down
+
+}
+
+
 dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dirname}
 
 cd schellar
 go test -v -short ./...
 
-docker-compose -f ../docker-compose.test.yml up -d
+trap testSchellar err exit
 
-trap 'docker-compose -f ../docker-compose.test.yml down' err exit
 
-sleep 5
-export BACKEND="postgres"
-
-export POSTGRES_HOST=127.0.0.1
-export POSTGRES_PORT=6432
-export POSTGRES_DB=schellar_test
-export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_MIGRATIONS_DIR="$(pwd)/migrations"
-go test -run Integration ./...
