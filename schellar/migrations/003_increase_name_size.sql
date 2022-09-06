64910ec17fc4c177f2226845841279b3e7f48a9e
Jozef Volak <jvolak@frinx.io>
2022-09-06 10:59:36 +0200

    Migration for increase workflow name

---
+++ schellar/migrations/003_increase_name_size.sql
@@ -0,0 +1,3 @@
+ALTER TABLE schedule ALTER COLUMN schedule_name TYPE varchar(100);
+ALTER TABLE schedule ALTER COLUMN workflow_name TYPE varchar(100);
+ALTER TABLE schedule ALTER COLUMN correlation_id TYPE varchar(100);
