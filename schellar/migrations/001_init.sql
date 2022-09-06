deb3ec5b9f44c8f62b2249df7a4c276c7a3e0ad7
Jozef Volak <jvolak@frinx.io>
2022-09-06 09:38:08 +0200

    Revert "Increase name size"
    This reverts commit d82f771fad69e9274315b8fee899c02613b5258d.

--- schellar/migrations/001_init.sql
+++ schellar/migrations/001_init.sql
@@ -1,8 +1,8 @@
 create table schedule(
-  schedule_name varchar(100) primary key,
+  schedule_name varchar(50) primary key,
   is_enabled boolean not null,
   workflow_status varchar(20) not null,
-  workflow_name varchar(100) not null,
+  workflow_name varchar(50) not null,
   workflow_version varchar(20) not null,
   workflow_context json not null,
   cron_string varchar(10) not null,
@@ -10,7 +10,7 @@ create table schedule(
   check_warning_seconds int not null,
   from_date timestamptz,
   to_date timestamptz,
-  correlation_id varchar(100),
+  correlation_id varchar(50),
   task_to_domain json,
   last_update timestamptz not null
 );
